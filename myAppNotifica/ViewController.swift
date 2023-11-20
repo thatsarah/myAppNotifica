ed069538e2a31e1be873af31ef672c1444500406
Sarah Rodrigues <150077782+thatsarah@users.noreply.github.com>
2023-11-20 08:56:36 -0300

    Initial Commit

---
+++ myAppNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myAppNotifica
+//
+//  Created by IFB-BIOTIC-08 on 20/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
